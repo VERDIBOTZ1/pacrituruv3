b427deb4f82271cf3c9065a08f1d5c2d3c47e528
Fachri Botz <94295494+Ipulpachri@users.noreply.github.com>
2022-04-29 08:53:27 +0700

    Update panggilzivfurr.js

--- plugins/panggilzivfurr.js
+++ plugins/panggilzivfurr.js
@@ -1,9 +1,9 @@
 let fs = require('fs')
 let handler = async (m) => {
 let stc = fs.readFileSync('./src/tag.webp')
-conn.fakeReply(m.chat, stc, '6281588866902@s.whatsapp.net', '*Kenapa Tag Owner Saya*')
+conn.fakeReply(m.chat, stc, '6285713041886@s.whatsapp.net', '*Kenapa Tag Owner Saya*')
 }
-handler.customPrefix = /^(@6285158866902)$/i
+handler.customPrefix = /^(@6285713041886)$/i
 handler.command = new RegExp
 
 module.exports = handler
