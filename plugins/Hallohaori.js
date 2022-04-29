64cffb618a85f16f812e858c4961e86308d4c90c
Fachri Botz <94295494+Ipulpachri@users.noreply.github.com>
2022-04-29 07:58:58 +0700

    Update Hallohaori.js

--- plugins/Hallohaori.js
+++ plugins/Hallohaori.js
@@ -1,10 +1,10 @@
 let fs = require('fs')
 let handler = async (m) => {
 let stc = fs.readFileSync('./sticker/Hallohaori.webp')
-conn.fakeReply(m.chat, stc, '0@s.whatsapp.net', '*✧───────···────────✧*\n*Hallo Juga Kak Haori Kangen Nih* \n *Gimana Kabar Nya? >~<*', 'status@broadcast')
+conn.fakeReply(m.chat, stc, '0@s.whatsapp.net', '*✧───────···────────✧*\n*Hallo Juga Kak, Fachri Kangen Nih* \n *Gimana Kabar Nya? >~<*', 'status@broadcast')
 }
 
-handler.customPrefix = /^(Hallo Haori|Hallo haori|hi haori|Hi Haori)$/i
+handler.customPrefix = /^(Hallo Fachri|Hallo fachri|hi fachri|Hi Fachri)$/i
 handler.command = new RegExp
 
 module.exports = handler
