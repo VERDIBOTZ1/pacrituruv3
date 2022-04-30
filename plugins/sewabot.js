c46b811196da3af085dcf5a76fffe7bd00ba5c2d
Fachri Botz <94295494+Ipulpachri@users.noreply.github.com>
2022-04-30 20:26:46 +0700

    Update sewabot.js

--- plugins/sewabot.js
+++ plugins/sewabot.js
@@ -8,7 +8,7 @@ let handler = async(m, { conn }) => {
 Silahkan klik pada "List Harga" untuk melihat list.
 
 Pembayaran:
-Dana : 6285158866902
+Dana : 6285713041886
 `.trim()
     const button = {
         buttonText: 'List Harga',
