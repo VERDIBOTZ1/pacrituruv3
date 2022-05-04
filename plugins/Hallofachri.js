e42c8c08d45b12ee8d2835855f39f1f84fd499e2
Fachri Botz <94295494+Ipulpachri@users.noreply.github.com>
2022-05-04 09:08:42 +0700

    Update and rename Hallohaori.js to Hallofachri.js

--- plugins/Hallofachri.js
+++ plugins/Hallofachri.js
@@ -1,7 +1,7 @@
 let fs = require('fs')
 let handler = async (m) => {
 let stc = fs.readFileSync('./sticker/Hallohaori.webp')
-conn.fakeReply(m.chat, stc, '0@s.whatsapp.net', '*✧───────···────────✧*\n*Hallo Juga Kak, Fachri Kangen Nih* \n *Gimana Kabar Nya? >~<*', 'status@broadcast')
+conn.fakeReply(m.chat, stc, '0@s.whatsapp.net', '*✧───────···────────✧*\n*Hallo Juga Kak* \n *Gimana Kabar Nya Kak? ><.*', 'status@broadcast')
 }
 
 handler.customPrefix = /^(Hallo Fachri|Hallo fachri|hi fachri|Hi Fachri)$/i
