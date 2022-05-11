2e757e533c1a793e41e52347e72fa39add669c7d
kurang turu <95448883+VERDIBOTZ1@users.noreply.github.com>
2022-05-11 12:31:55 +0700

    Update and rename Hallofachri.js to Hallofahcri.js

--- plugins/Hallofahcri.js
+++ plugins/Hallofahcri.js
@@ -4,7 +4,7 @@ let stc = fs.readFileSync('./sticker/Hallohaori.webp')
 conn.fakeReply(m.chat, stc, '0@s.whatsapp.net', '*✧───────···────────✧*\n*Hallo Juga Kak* \n *Gimana Kabar Nya Kak? Semoga modar hahaha.*', 'status@broadcast')
 }
 
-handler.customPrefix = /^(Hallo|Hallo|hi|Hi)$/i
+handler.customPrefix = /^(Hallo verdi|Hallo verdi|hi verdi|Hi verdi)$/i
 handler.command = new RegExp
 
 module.exports = handler
