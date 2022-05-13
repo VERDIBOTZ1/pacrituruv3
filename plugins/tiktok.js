6deb9463ac4a95c6da555904217fe469d2323cbe
kurang turu <95448883+VERDIBOTZ1@users.noreply.github.com>
2022-05-13 12:52:41 +0700

    Update tiktok.js

--- plugins/tiktok.js
+++ plugins/tiktok.js
@@ -1,14 +1,16 @@
-let fetch = require('node-fetch')
-let fs = require('fs')
-let handler = async(m, { conn, usedPrefix, text, command }) => {
-    if (!text) throw `Harap masukkan URL sebagai parameter.\n\nContoh: ${usedPrefix + command} https://vt.tiktok.com/ZSeSCAN1W/`
-    let res = await fetch(global.API('rey', '/api/download/tiktok', { url: text }, 'apikey'))
-    if (!res.ok) throw await `${res.status} ${res.statusText}`
-    let json = await res.json()
-    await conn.sendButtonVid(m.chat, json.result.nowatermark, 'Nih Kak', watermark, 'Thanks', `Thanks`, m)
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+  if (!text) throw `Teks nya mana kak?\nContoh :\n${usedPrefix+command} Hai | ${namalu}`
+  let [t1, t2] = text.split`|`
+  conn.sendFile(m.chat, global.API('https://some-random-api.ml', '/canvas/youtube-comment', {
+    avatar: await conn.getProfilePicture(m.sender).catch(_ => ''),
+    comment: t1,
+    username: t2
+  }), 'yt-comment.png', 'Here is your comment', m)
 }
-handler.command = /^tiktok$/i
-handler.tags = ['downloader']
-handler.help = ['tiktok']
-handler.limit = true
+
+handler.help = ['ytcomment <comment>']
+handler.tags = ['maker']
+
+handler.command = /^(ytcomment)$/i
+
 module.exports = handler
