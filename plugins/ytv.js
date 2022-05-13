d75dc2ff9168284e044582766ddf98a9a39736e8
kurang turu <95448883+VERDIBOTZ1@users.noreply.github.com>
2022-05-13 09:51:38 +0700

    Update ytv.js

--- plugins/ytv.js
+++ plugins/ytv.js
@@ -1,47 +1,16 @@
-let limit = 30
-let fetch = require('node-fetch')
-const { servers, ytv } = require('../lib/y2mate')
-let handler = async (m, { conn, args, isPrems, isOwner, usedPrefix, command }) => {
-  if (!args || !args[0]) throw 'uhm... urlnya mana?'
-  let chat = global.db.data.chats[m.chat]
-  let server = (args[1] || servers[0]).toLowerCase()
-  try {
-    let { dl_link, thumb, title, filesize, filesizeF } = await ytv(args[0], servers.includes(server) ? server : servers[0])
-    let isLimit = (isPrems || isOwner ? 99 : limit) * 1024 < filesize
-    m.reply(isLimit ? `Ukuran File: ${filesizeF}\nUkuran file diatas ${limit} MB, download sendiri: ${dl_link}` : global.wait)
-    let _thumb = {}
-    try { _thumb = { thumbnail: await (await fetch(thumb)).buffer() } }
-    catch (e) { }
-    if (!isLimit) conn.sendFile(m.chat, dl_link, '', `
-┏┉⌣ ┈̥-̶̯͡..̷̴✽̶┄┈┈┈┈┈┈┈┈┈┈┉┓
-┆ *YOUTUBE MP4*
-└┈┈┈┈┈┈┈┈┈┈┈⌣ ┈̥-̶̯͡..̷̴✽̶⌣ ✽̶
-
-*💌 Judul:* ${title}
-*✒️ Type:* MP4
-*📥 Ukuran File:* ${filesizeF}
-  `.trim(), m, 0, {
-   asDocument: chat.useDocument
-      ._thumb,
-    })
-  } catch (e) {
-    return await conn.sendButton(m.chat, 'Server Error', '', 'COBA LAGI', `${usedPrefix + command} ${args[0]}`)
-  }
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+  if (!text) throw `Teks nya mana kak?\nContoh :\n${usedPrefix+command} Hai | ${namalu}`
+  let [t1, t2] = text.split`|`
+  conn.sendFile(m.chat, global.API('https://some-random-api.ml', '/canvas/youtube-comment', {
+    avatar: await conn.getProfilePicture(m.sender).catch(_ => ''),
+    comment: t1,
+    username: t2
+  }), 'yt-comment.png', 'Here is your comment', m)
 }
-handler.help = ['mp4', 'v', ''].map(v => 'yt' + v + ` <url> [server: ${servers.join(', ')}]`)
-handler.tags = ['downloader']
-handler.command = /^yt(v|mp4)?$/i
-handler.owner = false
-handler.mods = false
-handler.premium = false
-handler.group = false
-handler.private = false
 
-handler.admin = false
-handler.botAdmin = false
+handler.help = ['ytcomment <comment>']
+handler.tags = ['maker']
 
-handler.fail = null
-handler.exp = 0
-handler.limit = true
+handler.command = /^(ytcomment)$/i
 
 module.exports = handler
