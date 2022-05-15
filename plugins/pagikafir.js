5ecfcd1f49ed96fad43cbb998542f9b28f90a8aa
kurang turu <95448883+VERDIBOTZ1@users.noreply.github.com>
2022-05-15 22:32:39 +0700

    Update pagikafir.js

--- plugins/pagikafir.js
+++ plugins/pagikafir.js
@@ -4,7 +4,7 @@ let stc = fs.readFileSync('./sticker/.webp')
 conn.fakeReply(m.chat, stc, '0@s.whatsapp.net', '*BAGUS KAH BEGITU DECK?*', 'status@broadcast')
 }
 
-handler.customPrefix = /^(p|P)$/i
+handler.customPrefix = /^(gblk|gblk)$/i
 handler.command = new RegExp
 
 module.exports = handler
