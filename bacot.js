bda64107e0ceb7221b31aec09ff6ae0c21bd8df0
kurang turu <95448883+VERDIBOTZ1@users.noreply.github.com>
2022-05-16 18:00:17 +0700

    Update bacot.js

--- bacot.js
+++ bacot.js
@@ -4,7 +4,7 @@ let stc = fs.readFileSync('./sticker/bacot.webp')
 conn.fakeReply(m.chat, stc, '0@s.whatsapp.net', '*SOK JAGO KENA ULTI MAK NAGIS*', 'status@broadcast')
 }
 
-handler.customPrefix = /^(jancok|kontol)$/i
+handler.customPrefix = /^(jancok|konto)$/i
 handler.command = new RegExp
 
 module.exports = handler
